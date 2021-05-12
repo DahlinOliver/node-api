86e33dfdae7f3cb88ffd7cc1742cb3b30d0cd384
DahlinOliver <oliver.dahlin@student.kyh.se>
2021-05-12 09:57:57 +0200

    Added vowels api

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 
 // Lyssnar på porten 1717
 app.listen(1717);
+app.use(express.json())
+
+app.use(express.static('public'))
 
 
 
@@ -15,16 +18,32 @@ app.listen(1717);
 // Ger ett random tal mellan 0 och de ID man ger i URL:en
 // https://lodash.com/docs/4.17.15#random
 // https://stackoverflow.com/questions/42033209/how-to-get-id-from-url-as-variable-in-node-js
-app.get("/api/custom_random/:id", (req, res) => {
-  
-  let id = req.params.id;
+app.get("/api/custom_random/:num", (req, res) => {
+  // /api/custom_random/hej
+  let id = req.params.num;
   res.send({ num: _.random(0, id) });
 });
-
 
 // Ger ett random tal mellan 0 - 1023
 app.get("/api/random", (req, res) => {
   res.send({ num: _.random(0, 1023) });
+});
+
+// https://www.w3resource.com/javascript-exercises/javascript-function-exercise-7.php
+app.get('/api/vowels/:word', (req, res) => {
+	let id = req.params.word;
+	function vowel_count(string) {
+		var vowel_list = 'aeiouAEIOU'
+		var vcount = 0
+
+		for (var i = 0; i < string.length; i++) {
+			if (vowel_list.indexOf(string[i]) !== -1) {
+				vcount += 1
+			}
+		}
+		return vcount
+	}
+	res.send({ vowels: vowel_count(id) })
 });
 
 
@@ -33,12 +52,12 @@ app.get("/api/random", (req, res) => {
 
 // Räknare, lägger till 1 för varje request
 let count = 0;
-app.get("/api/count", (req, res) => {
+app.get("/api/count/add", (req, res) => {
   count++;
-  res.send({ num: count });
+  res.send({ success: true });
 });
 
 // Nuvarande värde av count från räknaren
-app.get("/api/count/status", (req, res) => {
+app.get("/api/count/show", (req, res) => {
   res.send({ count });
 });
